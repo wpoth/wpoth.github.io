cab79efa421700784ae87c1e1eb0f834c5c6b4b4
wpoth <302758116@student.rocmondriaan.nl>
2025-05-17 16:31:05 +0200

    updated header for mobile and minor changes

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("menu-toggle");
+const menu = document.getElementById("mobile-menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
