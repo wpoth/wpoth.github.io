3a0fc91d32832210876f7a9380cdb70ac4454fd3
Wesley <w.poth1001@gmail.com>
2026-06-02 14:58:20 +0200

    removed blur circles, didnt work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,21 +30,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="relative min-h-full flex flex-col overflow-x-hidden bg-[#fcfcfc] text-black">
-
-        {/* AMBIENT BACKGROUND LAYERS */}
-        <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
-
-          {/* top glow */}
-          <div className="absolute -top-40 left-1/2 h-[600px] w-[600px] -translate-x-1/2 rounded-full bg-orange-400/25 blur-[120px] animate-pulse" />
-
-          {/* top glow */}
-          <div className="absolute -top-40 left-1/2 h-[600px] w-[600px] -translate-x-1/2 rounded-full bg-orange-400/25 blur-[120px] animate-pulse" />
-
-          {/* left glow */}
-          <div className="absolute top-1/3 left-[-250px] h-[500px] w-[500px] rounded-full bg-amber-300/15 blur-[120px]" />
-          
-        </div>
-
         {/* MAIN APP */}
         <ClientLayout>{children}</ClientLayout>
       </body>
